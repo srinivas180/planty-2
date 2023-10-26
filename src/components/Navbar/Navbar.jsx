5e6f65e99b3a1342065b84c3a67a14215e02931a
Srinivas <srinivas365@proton.me>
2023-10-26 16:08:00 +0530

    style: removed unnecessary wrapper

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,26 +3,24 @@ import "./Navbar.css";
 export function Navbar() {
     return (
         <div className="container">
-            <div className="wrapper">
-                <div className="left">
-                    <h1 className="title">Planty</h1>
-                </div>
+            <div className="left">
+                <h1 className="title">Planty</h1>
+            </div>
 
-                <div className="right">
-                    <nav className="nav">
-                        <ul className="nav__list">
-                            <li className="nav__item">Products</li>
-                            <li className="nav__item">Wishlist</li>
-                            <li className="nav__item">Cart</li>
-                        </ul>
-                    </nav>
-
-                    <ul className="options">
-                        <li className="options__item">Search</li>
-                        <li className="options__item">Login</li>
-                        <li className="options__item">Sign Up</li>
+            <div className="right">
+                <nav className="nav">
+                    <ul className="nav__list">
+                        <li className="nav__item">Products</li>
+                        <li className="nav__item">Wishlist</li>
+                        <li className="nav__item">Cart</li>
                     </ul>
-                </div>
+                </nav>
+
+                <ul className="options">
+                    <li className="options__item">Search</li>
+                    <li className="options__item">Login</li>
+                    <li className="options__item">Sign Up</li>
+                </ul>
             </div>
         </div>
     );
