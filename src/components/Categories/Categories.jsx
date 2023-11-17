083a0813c30b690f248c292eb152e27259ca41d8
Srinivas <srinivas365@proton.me>
2023-11-17 11:52:27 +0530

    on category click on home page navigate to products page and select choosed category

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,28 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
 
-import { fetchCategories } from "../../slices/productsSlice";
+import {
+    fetchCategories,
+    setCategory,
+    setFilters,
+} from "../../slices/productsSlice";
 
 import "./Categories.css";
 
 export function Categories() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const categories = useSelector((state) => state.products.categories);
+
+    function handleCategoryClick(clickedCategoryIndex) {
+        // reset selected categories
+        dispatch(setFilters({ filterName: "categories", filterValue: [] }));
+
+        dispatch(setCategory(clickedCategoryIndex));
+
+        navigate("/products");
+    }
 
     useEffect(() => {
         dispatch(fetchCategories());
@@ -17,8 +32,11 @@ export function Categories() {
         <div className="categories-container">
             <h2 className="categories-heading">Categories</h2>
             <div className="categories">
-                {categories.slice(0, 3).map((category) => (
-                    <div className="category">
+                {categories.slice(0, 3).map((category, index) => (
+                    <div
+                        className="category"
+                        onClick={() => handleCategoryClick(index)}
+                    >
                         <img
                             className="category__image"
                             src={category.imageLink}
