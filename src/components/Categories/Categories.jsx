0c4cacd0cd03652f4aeb0f7aa1fc161e905a5794
Srinivas <srinivas365@proton.me>
2023-11-22 12:50:07 +0530

    feature: show loading icon while fetching categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 
+import { Loading } from "../Loading/Loading";
 import {
     fetchCategories,
     setCategory,
@@ -14,6 +15,7 @@ export function Categories() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const categories = useSelector((state) => state.products.categories);
+    const loadingStatus = useSelector((state) => state.products.status);
 
     function handleCategoryClick(clickedCategoryIndex) {
         // reset selected categories
@@ -31,22 +33,30 @@ export function Categories() {
     return (
         <div className="categories-container">
             <h2 className="categories-heading">Categories</h2>
-            <div className="categories">
-                {categories.slice(0, 3).map((category, index) => (
-                    <div
-                        className="category"
-                        onClick={() => handleCategoryClick(index)}
-                    >
-                        <img
-                            className="category__image"
-                            src={category.imageLink}
-                            alt={category.altText}
-                        />
-                        <div className="category__text">
-                            {category.categoryName}
+            <div
+                className={`categories ${
+                    loadingStatus === "loading" ? "row--center" : ""
+                }`}
+            >
+                {loadingStatus === "loading" ? (
+                    <Loading size="120" />
+                ) : (
+                    categories.slice(0, 3).map((category, index) => (
+                        <div
+                            className="category"
+                            onClick={() => handleCategoryClick(index)}
+                        >
+                            <img
+                                className="category__image"
+                                src={category.imageLink}
+                                alt={category.altText}
+                            />
+                            <div className="category__text">
+                                {category.categoryName}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))
+                )}
             </div>
         </div>
     );
